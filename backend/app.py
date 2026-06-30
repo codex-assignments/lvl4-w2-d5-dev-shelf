f3437fb0522db7e40045224d807923761ec8f658
Ashley Flynn <ashnflynn@gmail.com>
2026-06-30 12:30:19 -0500

    added update and delete

--- backend/app.py
+++ backend/app.py
@@ -4,18 +4,50 @@ from shelf_data import shelf_items
 app=Flask(__name__)
 CORS(app)
 
-# set up endpoints for the api, add GET methods for readability
+# set up endpoints for the api
 
 #get items
 @app.route("/api/shelf", methods=["GET"])
 def get_shelf():
     return {"items": shelf_items}, 200
 
-#target a specific item, in case of updating that dictionary or deleting
+#target a specific item, in case of updating that dictionary or deleting:
 
+#delete
+@app.route("/api/shelf/<int:item_id>", methods=["DELETE"])
+def delete_from_shelf(item_id):
+    global shelf_items #to modify the global variable
+    
+    # does item exist
+    target_item = next((item for item in shelf_items if item["id"] == item_id), None)
+    if not target_item:
+        return {"error": f"Tool with ID {item_id} not found."}, 404
 
+    # filter out the item, shelf_items = all items but the target item
+    shelf_items = [item for item in shelf_items if item["id"] != item_id]
+    
+    return {"message": f"'{target_item['title']}' removed from shelf."}, 200
 
-#post items
+# update with put
+@app.route("/api/shelf/<int:item_id>", methods=["PUT"])
+def update_shelf_item(item_id):
+    body=request.get_json(force=True) or {}
+    target_item=next((item for item in shelf_items if item["id"] == item_id), None)
+    if not target_item: 
+        return {"error": f"Tool with ID {item_id} not found."}, 404
+    #overwrite with provided values in body and if nothing is provided in the field, use old values
+    target_item["title"] = body.get("title", target_item["title"])
+    target_item["category"] = body.get("category", target_item["category"])
+    target_item["description"] = body.get("description", target_item["description"])
+    target_item["quick_ref"] = body.get("quick_ref", target_item["quick_ref"])
+    target_item["resources"] = body.get("resources", target_item["resources"])
+
+    return {
+        "message": f"'{target_item['title']}' updated successfully!",
+        "item": target_item
+    }, 200
+
+#post new tool
 @app.route("/api/shelf", methods=["POST"])
 def add_to_shelf():
     # get_json parses into python dictionary from raw text in body, force=True ignores header mistakes
